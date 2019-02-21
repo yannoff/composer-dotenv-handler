75a9071f24cb220d33ef5ac774ddb0cad9921fcb
Yannoff <yannoff.it@gmail.com>
2019-02-21 18:57:21 +0100

    Implement user-defined configuration
    - Add support for config via `extra` section
    - Update documentation with available options

--- README.md
+++ README.md
@@ -14,9 +14,29 @@ Then, set up your `composer.json` accordingly, as in the following example:
 
 ```json
 ...
-"scripts": {
-    "post-install-cmd": "Yannoff\\DotenvHandler\\ScriptHandler::updateEnvFile"
-}
+    "scripts": {
+        "post-install-cmd": "Yannoff\\DotenvHandler\\ScriptHandler::updateEnvFile"
+    }
 ...
 
 ```
+
+## Advanced options
+
+Options may be passed via the `extra` section of the composer.json :
+
+```yaml
+    "extra": {
+        "yannoff-dotenv-handler": {
+            // options here
+        }
+    }
+```
+
+### Available options
+
+|**Name**     |**Default value**|**Description**
+|:-----------:|:---------------:|:--------------:
+|file         | .env            |The name of the auto-generated *dotenv* file
+|dist-file    | .env.dist       |The name of the *dist* file
+|keep-outdated| true            |Keep values in the *env file* that are not anymore in the *dist file*
--- src/ScriptHandler.php
+++ src/ScriptHandler.php
@@ -4,19 +4,43 @@ namespace Yannoff\DotenvHandler;
 
 use Composer\Script\Event;
 
+/**
+ * Class ScriptHandler
+ *
+ * @package Yannoff\DotenvHandler
+ */
 class ScriptHandler
 {
+    /** @var string The key in extra where user can configure the script */
+    const EXTRA_CONFIG_KEY = 'yannoff-dotenv-handler';
+
+    /**
+     * Main entry point of the script, hte method called by composer
+     *
+     * @param Event $event
+     */
     public static function updateEnvFile(Event $event)
     {
-        //$extras = $event->getComposer()->getPackage()->getExtra();
+        $extras = $event->getComposer()->getPackage()->getExtra();
 
         $processor = new Processor($event->getIO());
 
-        $config = [
-            'file' => '.env',
-            'dist-file' => '.env.dist',
-        ];
+        $config = array_merge(self::getDefaults(), $extras[self::EXTRA_CONFIG_KEY] ?? []);
 
         $processor->processFile($config);
+    }
+
+    /**
+     * Provide default config values
+     *
+     * @return array
+     */
+    protected static function getDefaults()
+    {
+        return [
+            'file' => '.env',
+            'dist-file' => '.env.dist',
+            'keep-outdated' => true,
+        ];
     }
 }
